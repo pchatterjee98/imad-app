ec4fbab5d9597d29970263984f1d3bf6f8f18efa
pchatterjee98 <36557972+pchatterjee98@users.noreply.github.com>
2018-02-19 22:55:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: `This is Partho's heading for article one`,
+    heading: 'Article One',
+    date: 'Feb 19,2018'
+    content: `
+    <p>
+        This will be a paragraph of text -- 1.This will be a paragraph of text -- 1.
+        This will be a paragraph of text -- 1.
+        This will be a paragraph of text -- 1.
+    </p>
+    <p>
+        This will be a paragraph of text -- 2. This will be a paragraph of text -- 2.
+        This will be a paragraph of text -- 2.
+    </p>
+    `
+};
+
+
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmltemplate = `
+    <html>
+        <head>
+            <title> ${title} </title>
+            <meta name="viewport" content="width=device-width , initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr />
+                <h3> ${heading} </h3>
+                <div>
+                    ${date} 
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate; 
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
+   res.send(createTemplate(articleOne)); 
 });
 app.get('/article-two',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));  

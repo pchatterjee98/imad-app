88e73ce317bae9cd4e00ece3dc032659a26a1881
pchatterjee98 <36557972+pchatterjee98@users.noreply.github.com>
2018-02-20 09:55:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,10 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML= 'New value';
+
+//move the image by clicking on it
+
+var img = document.getElementById('Myimage');
+img.onclick = function () {
+    img.style.marginLeft = '100px'; 
+};

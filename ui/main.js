2c9078be912f30d0e2744e07aa53d479f59fc66a
pchatterjee98 <36557972+pchatterjee98@users.noreply.github.com>
2018-02-20 10:02:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,12 @@ element.innerHTML= 'New value';
 //move the image by clicking on it
 
 var img = document.getElementById('Myimage');
+
+marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft +10 ;
+    img.style.marginLeft = marginLeft+'px';
+}
 img.onclick = function () {
-    img.style.marginLeft = '100px'; 
+    var interval = setInterval(moveRight,100); 
 };
